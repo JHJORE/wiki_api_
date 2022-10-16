426b5f8a8e0023c34eae08d3b6e5cfd96282c2ba
Joergen Jore <ntnu@berkeley.edu>
2022-10-16 16:57:26 -0700

    cleaning up for tkinter

--- sql_app/main.py
+++ sql_app/main.py
@@ -14,7 +14,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
 def get_db():
     db = SessionLocal()
@@ -23,34 +23,34 @@ def get_db():
     finally:
         db.close()
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    data = {
-        "page": "Home page"
-    }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+# @app.get("/", response_class=HTMLResponse)
+# async def home(request: Request):
+#     data = {
+#         "page": "Home page"
+#     }
+#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
-@app.get("/page/{page_name}", response_class=HTMLResponse)
-async def page(request: Request, page_name: str):
-    data = {
-        "page": page_name
-    }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+# @app.get("/page/{page_name}", response_class=HTMLResponse)
+# async def page(request: Request, page_name: str):
+#     data = {
+#         "page": page_name
+#     }
+#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 @app.post("/wiki/", response_model=schemas.Wiki)
 def create_Wiki(Wiki: schemas.WikiCreate, db: Session = Depends(get_db)):
     return crud.create_Wiki(db=db, Wiki=Wiki)
 
-@app.get("/wiki/{Wikiid}", response_model=schemas.Wiki)
-def read_Wiki(Wikiid: int, db: Session = Depends(get_db)):
-    print("success")
-    db_Wiki = crud.get_Wiki(db, Wikiid=Wikiid)
-    if db_Wiki is None:
-        raise HTTPException(status_code=404, detail="Wiki not found")
-    return db_Wiki
+# @app.get("/wiki/{Wikiid}", response_model=schemas.Wiki)
+# def read_Wiki(Wikiid: int, db: Session = Depends(get_db)):
+#     print("success")
+#     db_Wiki = crud.get_Wiki(db, Wikiid=Wikiid)
+#     if db_Wiki is None:
+#         raise HTTPException(status_code=404, detail="Wiki not found")
+#     return db_Wiki
     
 @app.get("/wiki/titles/{WikiTitle}", response_model=schemas.Wiki)
 def read_Wiki_By_Name(WikiTitle: str, db: Session = Depends(get_db)):
